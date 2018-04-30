abf58644c9e6972d18e6404e1d25a35b8b8addbf
JeonSeongBae <tjdqo6042@naver.com>
2018-04-30 17:58:24 +0900

    make function addTodo

--- TP1_Skeleton/TP1_SKELETON_JavaScript.js
+++ TP1_Skeleton/TP1_SKELETON_JavaScript.js
@@ -5,3 +5,17 @@ function Open_AddToDo(){
 function Close_AddToDo(){
   document.getElementById('modal').style.display = "none";
 }
+
+function Add_AddToDo(){
+  var content_AddToDo = document.getElementsByClassName('content_AddToDo');
+  var temp1 = content_AddToDo[0].value;
+  var temp2 = content_AddToDo[1].value;
+  var temp3 = content_AddToDo[2].value;
+  alert("day : "+temp1+"\ntitle : "+temp2+"\ncontent : "+temp3);
+  Insert_AddToDo(temp1, temp2, temp3);
+  Close_AddToDo();
+}
+
+function Insert_AddToDo(temp1, temp2, temp3){
+  
+}
